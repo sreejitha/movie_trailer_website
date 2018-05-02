1c15afd34ac4643e2c853f5ae92fc5a75a4a64d3
Deepak Chandran <deepak@radiantgenomics.com>
2018-05-01 22:20:42 -0700

    following pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,8 +4,8 @@ import media
 import fresh_tomatoes
 
 MOVIE_1 = media.Movie("Dear Zindagi",
-                      "The plot centres on a budding cinematographer named Kaira"
-                      + "and her conversations with her psychologist",
+                      "The plot centres on a budding cinematographer named Kaira" +
+                      "and her conversations with her psychologist",
                       "https://upload.wikimedia.org/wikipedia/en/9/9e/Dear_Zindagi_poster.jpg",
                       "https://www.youtube.com/watch?v=5DkO7ksXY8E")
 
@@ -15,27 +15,27 @@ MOVIE_2 = media.Movie("Pranayavarnangal",
                       "https://www.youtube.com/watch?v=xL1aW1PupEk")
 
 MOVIE_3 = media.Movie("Lagaan",
-                      "The story revolves around a small village whose inhabitants"
-                      + "are challenged to a game of cricket by their officer for tax waiver",
+                      "The story revolves around a small village whose inhabitants" +
+                      "are challenged to a game of cricket by their officer for tax waiver",
                       "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Lagaan.jpg/220px-Lagaan.jpg",
                       "https://www.youtube.com/watch?v=rxWhke_3fbQ")
 
 MOVIE_4 = media.Movie("Dil Chahta Hai",
-                      "Set in modern-day urban Mumbai and Sydney the story is about"
-                      + "a major period of transition in the lives of three young friends.",
+                      "Set in modern-day urban Mumbai and Sydney the story is about" +
+                      "a major period of transition in the lives of three young friends.",
                       "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Dil_Chahta_Hai.jpg/220px-il_Chahta_Hai.jpg",
                       "https://www.youtube.com/watch?v=m13b25V0B10")
 
 MOVIE_5 = media.Movie("Swades",
-                      "The film is based on the true life story of a Non-resident Indian returning"
-                      + "to his roots",
+                      "The film is based on the true life story of a Non-resident Indian returning" +
+                      "to his roots",
                       "https://upload.wikimedia.org/wikipedia/en/8/85/Swades_poster.jpg",
                       "https://www.youtube.com/watch?v=vc7AZNWvs0M")
 
 MOVIE_6 = media.Movie("Kal Ho Na Ho",
-                      "This movie narrates the story of Naina Catherine Kapur,"
-                      + "a pessimistic and uptight MBA student who falls in love"
-                      + "with her neighbour Aman Mathur",
+                      "This movie narrates the story of Naina Catherine Kapur," +
+                      "a pessimistic and uptight MBA student who falls in love" +
+                      "with her neighbour Aman Mathur",
                       "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/KalHoNaaHo.jpg/220px-KalHoNaaHo.jpg",
                       "https://www.youtube.com/watch?v=tVMAQAsjsOU")
 
