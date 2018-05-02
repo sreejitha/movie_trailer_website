ca689872ee35bd74a032c56de2134dfcfed85885
Deepak Chandran <deepak@radiantgenomics.com>
2018-05-02 10:58:01 -0700

    modified for lines too long

--- media.py
+++ media.py
@@ -1,18 +1,25 @@
-"""This module is to store favorite movies and play their trailers when selected"""
+"""This module is to store favorite movies and play their trailers when
+selected"""
 import webbrowser
 
 
 class Movie(object):
-    """The class is used to initialize the movie object with all its properties including
-    movie title, trailer, storyline and poster image. It also has a method to open up a browser to
+    """The class is used to initialize the movie object with all its
+    properties including movie title, trailer, storyline and poster image.
+    It also has a method to open up a browser to
     show the selected movie's trailer"""
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """this method opens up a browser window displaying youtube trailer of the movie"""
+        """this method opens up a browser window displaying youtube
+        trailer of the movie"""
         webbrowser.open(self.trailer_youtube_url)
